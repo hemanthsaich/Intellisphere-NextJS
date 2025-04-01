4fa5b6155648bf7f5ee0ba9e7b042feca322a05c
hemanthsaich <HSai@ESTUATE.COM>
2025-04-01 19:46:47 +0530

    sonarscan-report

--- jest.config.js
+++ jest.config.js
@@ -10,6 +10,16 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
+  collectCoverage: true, // Enable code coverage
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}', // Include all JavaScript and TypeScript files
+    '!**/node_modules/**', // Exclude node_modules
+    '!**/.next/**', // Exclude Next.js build directory
+    '!**/jest.config.js', // Exclude Jest config file
+    '!**/jest.setup.js', // Exclude Jest setup file
+  ],
+  coverageDirectory: '<rootDir>/coverage', // Output directory for coverage reports
+  coverageReporters: ['json', 'lcov', 'text', 'clover'], // Coverage report formats
 };
 
 module.exports = createJestConfig(customJestConfig);
